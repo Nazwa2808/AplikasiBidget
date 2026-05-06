13af25c6087d7176c2550f797c7748ea52f3ddf5
Nazwa Olivia Putri <nazwagomez174@gmail.com>
2026-05-06 14:23:29 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "gaurgupxooqjxpowjubf.supabase.co",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
